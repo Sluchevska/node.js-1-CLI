3a3c4e0d12ae831da2bf66604b5c944191a5df35
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-10-31 14:18:39 +0000

    update

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log(process.env)
+const path = require('path');
+const contactsPath = path.resolve('./db/contacts.json')
+console.log(contactsPath)
+
+const fs = require('fs')
+fs.readFile(contactsPath, 'utf8', (error, data) => {
+    if (error) {
+        console.log(error)
+    }
+    console.log(data)
+})
+
