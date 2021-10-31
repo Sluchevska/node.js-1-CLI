674d511e5f516a5aa88b6bcec20033563290ce5f
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-10-31 14:24:34 +0000

    update

--- index.js
+++ index.js
@@ -3,10 +3,31 @@ const contactsPath = path.resolve('./db/contacts.json')
 console.log(contactsPath)
 
 const fs = require('fs')
-fs.readFile(contactsPath, 'utf8', (error, data) => {
+
+
+function listContacts() {
+   fs.readFile(contactsPath, 'utf8', (error, data) => {
     if (error) {
         console.log(error)
     }
     console.log(data)
-})
+}) 
+}
+
+function getContactById(contactId) {
+ 
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
+
+
+export {
+    listContacts
+}
 
