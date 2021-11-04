e046c8e6a250494537bfdd5f687a555f407e6388
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-11-04 20:17:43 +0000

    update

--- index.js
+++ index.js
@@ -1,59 +1,53 @@
 const path = require('path');
-const contactsPath = path.resolve('./db/contacts.json')
+const contactsPath = path.resolve('./db/contacts.json');
 
-
-const fs = require('fs').promises
-
+const fs = require('fs').promises;
 
 async function listContacts() {
-   
-    try {
-        const data = await fs.readFile(contactsPath, 'utf8')
-        console.log(data)
-        return JSON.parse(data)
-    } catch (error) {
-        return console.log(error)
-    }
+  try {
+    const data = await fs.readFile(contactsPath, 'utf8');
+    console.log(data);
+    return JSON.parse(data);
+  } catch (error) {
+    return console.log(error);
+  }
 }
 
 //  listContacts()
 
 async function getContactById(contactId) {
-    const contacts = await listContacts();
-    const result = contacts.filter((contact) => contact.id === Number(contactId))
-    console.log(result)
-    return result
+  const contacts = await listContacts();
+  const result = contacts.filter(contact => contact.id === Number(contactId));
+  console.log(result);
+  return result;
 }
 
-getContactById(2)
+// getContactById(2)
 
 async function removeContact(contactId) {
-  try {
-        const data = await fs.unlink('contactID', )
-        console.log(data) 
-    } catch (error) {
-        console.log(error)
-    }
+  const contacts = await listContacts();
+  const result = contacts.filter(contact => contact.id !== Number(contactId));
+   await fs.writeFile(contactsPath, JSON.stringify(result, null, contactId))
+  console.log(result);
+  return result;
 }
-// removeContact(2)
+// removeContact(8);
 
 async function addContact(name, email, phone) {
- try {
-        const NewContact = `${data} name email phone `
-     await fs.writeFile(contactsPath,NewContact, 'utf8')
-        console.log(data) 
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    const NewContact = `${data} name email phone `;
+    await fs.writeFile(contactsPath, NewContact, 'utf8');
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // addContact(tess)
 
-
-module.exports =  {
-    listContacts,
-    getContactById,
-    removeContact,
-    addContact
-}
-
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
