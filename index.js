f575023a9f6fdd6cda7830f496affde272286813
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-11-05 20:42:24 +0000

    update

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const {
-        listContacts, getContactById, addContact, removeContact
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
 } = require('./contacts');
 
 const { Command } = require('commander');
@@ -14,39 +17,41 @@ program
 program.parse(process.argv);
 
 const argv = program.opts();
-    
-
-
 
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case 'list':
       listContacts()
-        .then((data) => console.table(data))
-      .catch(console.error)
+        .then(data => console.table(data))
+        .catch(console.error);
       break;
 
     case 'get':
       getContactById(id)
         .then(contact => {
           if (contact) {
-          console.table(contact)
+            console.table(contact);
           } else {
-            console.log('Contact not found')
-        }
-      }).catch(console.error)
+            console.log('Contact not found');
+          }
+        })
+        .catch(console.error);
       break;
 
     case 'add':
-      addContact(name, email, phone).then(contacts => {
-        console.table(contacts)
-      }).catch(console.error)
+      addContact(name, email, phone)
+        .then(contacts => {
+          console.table(contacts);
+        })
+        .catch(console.error);
       break;
 
     case 'remove':
-      removeContact(id).then(contact => {
-        console.table(contacts)
-      }).catch(console.error)
+      removeContact(id)
+        .then(contact => {
+          console.table(contact);
+        })
+        .catch(console.error);
       break;
 
     default:
@@ -54,4 +59,4 @@ function invokeAction({ action, id, name, email, phone }) {
   }
 }
 
-invokeAction(argv)
+invokeAction(argv);
